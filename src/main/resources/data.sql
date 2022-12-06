2c6453b150ee38b2b603cafb04abc43dd09f9f08
Ivana Kasikovic <ikasikovic@yahoo.com>
2022-12-06 23:50:34 +0100

    Fix conflicts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,9 @@ insert into role (name) values ('ROLE_client');
 insert into role (name) values ('ROLE_admin');
 insert into role (name) values ('ROLE_driver');
 
-
 INSERT INTO system_info (token_price) VALUES (5.0);
 
-INSERT INTO clients (id, email, password, name, surname, deleted, phone_number, city, tokens, photo, blocked, enabled)
-          VALUES (nextval('user_seq'), 'ivanaj0610@gmail.com', '$2a$10$iWm70CXU267iEgtasI.gGOYswU4qcaGKxa7rw/ZJtHnhdmukWqwWW', 'Ivana', 'Jankovic', false, '0654079380', 'Trebinje', 10, 'unknown.jpg', false, true);
+INSERT INTO clients (id, email, password, name, surname, deleted, phone_number, city,is_social_login, tokens, photo, blocked, enabled)
+          VALUES (nextval('user_seq'), 'ivanaj0610@gmail.com', '$2a$10$iWm70CXU267iEgtasI.gGOYswU4qcaGKxa7rw/ZJtHnhdmukWqwWW', 'Ivana', 'Jankovic', false, '0654079380', 'Trebinje',false, 10, 'unknown.jpg', false, true);
 
 insert into user_role (user_id, role_id) values (1, 1);
