d40463e1f76237c02e899da14fa5fe1a60b1887e
unknown <marijamaki26@gmail.com>
2023-01-10 14:11:59 +0100

    Resolve conflicts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,20 +29,17 @@ VALUES (nextval('user_seq'), 'driver@gmail.com', '$2a$10$iWm70CXU267iEgtasI.gGOY
 insert into user_role (user_id, role_id) values (1, 1);
 insert into user_role (user_id, role_id) values (2, 1);
 insert into user_role (user_id, role_id) values (3, 2);
+insert into user_role (user_id, role_id) values (4, 1);
+insert into user_role (user_id, role_id) values (5, 1);
+insert into user_role (user_id, role_id) values (6, 1);
+insert into user_role (user_id, role_id) values (7, 3);
+
 
 insert into notes (admin_id,driver_id,client_id,note) values (3,-1 ,1,'Neki pogrdan komentar');
-insert into grades (car_grade,driver_grade,comment,client,driver,drive_id) values (2,2,null ,1,7,1)
-INSERT INTO admins (id, email, password, name, surname, deleted, phone_number, city,is_social_login, enabled)
-VALUES (nextval('user_seq'), 'admin@gmail.com', '$2a$10$iWm70CXU267iEgtasI.gGOYswU4qcaGKxa7rw/ZJtHnhdmukWqwWW', 'Admin', 'Admin', false, '0654079380', 'Trebinje',false, true);
+insert into grades (car_grade,driver_grade,comment,client,driver,drive_id) values (2,2,null ,1,7,1);
 
-insert into user_role (user_id, role_id) values (2, 2);
 
-INSERT INTO clients (id, email, password, name, surname, deleted, phone_number, city,is_social_login, tokens, photo, blocked, enabled)
-VALUES (nextval('user_seq'), 'maki@gmail.com', '$2a$10$iWm70CXU267iEgtasI.gGOYswU4qcaGKxa7rw/ZJtHnhdmukWqwWW', 'Marija', 'Milosevic', false, '0654079380', 'Cuprija',false, 10, 'coka.jpg', false, true);
-
-insert into user_role (user_id, role_id) values (3, 1);
-
-insert into messages (sender, recipient, text) values (1,2,'sta sta');
-insert into messages (sender, recipient, text) values (2,1,'sta vam treba?');
-insert into messages (sender, recipient, text) values (1,2,'nista hehe');
-insert into messages (sender, recipient, text) values (3,2,'sta ima?');
+insert into messages (sender, recipient, text) values (1,3,'sta sta');
+insert into messages (sender, recipient, text) values (3,1,'sta vam treba?');
+insert into messages (sender, recipient, text) values (1,3,'nista hehe');
+insert into messages (sender, recipient, text) values (2,3,'sta ima?');
